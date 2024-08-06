b494950f2dc5c99d601e503f3763c74f598c4f03
Jindjeet <jindjeet2010@gmail.com>
2024-08-05 22:25:41 -0600

    add csvreader and create database

--- src/ca/ucalgary/edu/ensf380/CSVReader.java
+++ src/ca/ucalgary/edu/ensf380/CSVReader.java
@@ -1,38 +1,57 @@
 package ca.ucalgary.edu.ensf380;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
+/**
+ * Reads station data from a CSV file and monitors output files for train data updates.
+ * @author Jindjeet Cheema
+ * @version 1.0
+ */
 public class CSVReader {
-
-    public static List<Station> readStations(String csvFile) {
-        List<Station> stations = new ArrayList<>();
+    public static List<Station> createStationDatabase(String csvFile) {
+        // String csvFile = "C:\\Users\\jindjeet singh\\Desktop\\ENSF_380\\2nd\\Final_Project\\ENSF380_FinalProject - Copy\\data\\subway.csv"; // Path to your CSV file
         String line;
         String csvSplitBy = ",";
 
+        List<Station> stations = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            // Skip header line
+            // Read the header line
             br.readLine();
 
+            // Read each line of the CSV file
             while ((line = br.readLine()) != null) {
+                // Use comma as separator
                 String[] stationData = line.split(csvSplitBy);
-                int row = Integer.parseInt(stationData[0]);
-                String lineId = stationData[1];
-                int stationNumber = Integer.parseInt(stationData[2]);
-                String stationCode = stationData[3];
-                String stationName = stationData[4];
-                double x = Double.parseDouble(stationData[5]);
-                double y = Double.parseDouble(stationData[6]);
-                String commonStations = stationData.length > 7 ? stationData[7] : "";
 
-                stations.add(new Station(row, lineId, stationNumber, stationCode, stationName, x, y, commonStations));
+                // Parse the data: +1 since first column is row number
+                String trainLine = stationData[0+1];
+                int stationNumber = Integer.parseInt(stationData[1+1]);
+                String stationCode = stationData[2+1];
+                String stationName = stationData[3+1];
+                double x = Double.parseDouble(stationData[4+1]);
+                double y = Double.parseDouble(stationData[5+1]);
+                String commonStations = null;
+                if (stationData.length >= 8) {
+                    commonStations = stationData[6+1];
+
+                }
+
+                // Create a TrainStation object and add it to the list
+                Station station = new Station(trainLine, stationNumber, stationCode, stationName, x, y, commonStations);
+                stations.add(station);
             }
         } catch (IOException e) {
             e.printStackTrace();
+            System.exit(-1);
         }
+
+        // Print the list of stations
+        // for (Station station : stations) {
+        //     System.out.println(station);
+        // }
 
         return stations;
     }
